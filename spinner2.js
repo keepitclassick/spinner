c6fb25214b1d8cc6e86d5f2ba3872434927d19cf
keepitclassick <keepitclassick@gmail.com>
2021-11-02 14:35:54 -0400

    .

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,11 @@
 const spinMe = ['|   ,', '\r/   ', '\r-   ','\r\\   ','\r/   ', '\r-   ','\r\\   ','\r|   '];
 let int = 100;
 
-for (const move of spinMe) {
-  setTimeout(() => {
-    process.stdout.write(move);
-  }, int);
-  int += 200;
-}
+const spinner = function(arr) {
+  for (const move of spinMe) {
+    setTimeout(() => {
+      process.stdout.write(move);
+    }, int);
+    int += 200;
+  }
+};
