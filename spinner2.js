997c90f2535669c3982cb1d1c69a81edb4a8d3f6
keepitclassick <keepitclassick@gmail.com>
2021-11-02 14:37:16 -0400

    updated to call func for ex

--- spinner2.js
+++ spinner2.js
@@ -2,10 +2,12 @@ const spinMe = ['|   ,', '\r/   ', '\r-   ','\r\\   ','\r/   ', '\r-   ','\r\\  
 let int = 100;
 
 const spinner = function(arr) {
-  for (const move of spinMe) {
+  for (const move of arr) {
     setTimeout(() => {
       process.stdout.write(move);
     }, int);
     int += 200;
   }
 };
+
+spinner(spinMe);
